0c933f688d967937e9d6703cf164dca83c8cea42
nilakshi <nilakshinonis@gmail.com>
2024-06-10 15:17:58 +0530

    Format code

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -12,34 +12,46 @@ interface TableProps {
 }
 
 const Table: React.FC<TableProps> = ({ items, onCheckBoxChange }) => {
-  const handleCheckboxChange = (id: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    onCheckBoxChange(id, event.target.checked);
-  };
+  const handleCheckboxChange =
+    (id: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+      onCheckBoxChange(id, event.target.checked);
+    };
   return (
     <div>
-      <table style={{ border: "2px solid black", width: "50%"}}>
+      <table style={{ border: "2px solid black", width: "50%" }}>
         <thead>
           <tr>
             <th style={{ border: "1px solid black", padding: "8px" }}>Items</th>
-            <th style={{ border: "1px solid black", padding: "8px" }}>Status</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>
+              Status
+            </th>
           </tr>
         </thead>
         <tbody>
           {items.map((item) => (
             <tr key={item.id}>
-              <td style={ { border: "1px solid black", padding: "8px" }}>
-                <span style={{ marginLeft: "10px"}}>
-                  {item.isChecked ? <del style={{color:"red"}}>{item.title}</del> : item.title}
+              <td style={{ border: "1px solid black", padding: "8px" }}>
+                <span style={{ marginLeft: "10px" }}>
+                  {item.isChecked ? (
+                    <del style={{ color: "red" }}>{item.title}</del>
+                  ) : (
+                    item.title
+                  )}
                 </span>
-                </td>
-                <td style={{textAlign:"center", border: "1px solid black", padding: "8px"}}>
+              </td>
+              <td
+                style={{
+                  textAlign: "center",
+                  border: "1px solid black",
+                  padding: "8px",
+                }}
+              >
                 <input
                   type="checkbox"
                   checked={item.isChecked}
                   onChange={handleCheckboxChange(item.id)}
-                 
                 />
-             </td>
+              </td>
             </tr>
           ))}
         </tbody>
