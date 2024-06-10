a67bb3b66aa8aee3ddee76dfdb17f8873a005e9a
nilakshi <nilakshinonis@gmail.com>
2024-06-10 15:10:51 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import Table from './components/Table';
+
+interface Itable {
+  id: number;
+  title: string;
+  isChecked?: boolean;
+}
+
+const App: React.FC = () => {
+  const [items, setItems] = useState<Itable[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<number | null>(null);
+
+
+
+ 
+  useEffect(() => {
+  
+    const fetchData = async () => {
+      try {
+       
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+       
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+       
+        const data: Itable[] = await response.json();
+ 
+        const mappedData = data.map(todo => ({ id: todo.id, title: todo.title }));
+      
+        setItems(mappedData.slice(0, 10));
+      } catch (error) {
+
+        setError(error as Error);
+      } finally {
+       
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []); 
+
+  if (loading) return <div>Loading...</div>;
+  
+  if (error) return <div>Error: {error}</div>;
+
+
+  const handleCheckboxChange = (id: number, isChecked: boolean) => {
+    setItems(prevItems =>
+      prevItems.map(item =>
+        item.id === id ? { ...item, isChecked } : item
+      )
+    );
+  };
+  const completedItems = items.filter(items => items.isChecked).length;
+  const toBeCompletedItems = items.length - completedItems;
+
+ 
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <Table items={items} onCheckBoxChange={handleCheckboxChange} />
+      <h3 style={{border: "1px solid black", width:"20%", padding:"2px"}}>Completed Items count: {completedItems}</h3>
+      <h3 style={{border: "1px solid black", width:"20%", padding:"2px"}}>To do Items count: {toBeCompletedItems}</h3>
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Table.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+interface Itable {
+  id: number;
+  title: string;
+  isChecked?: boolean;
+}
+
+interface TableProps {
+  items: Itable[];
+  onCheckBoxChange: (id: number, isChecked: boolean) => void;
+}
+
+const Table: React.FC<TableProps> = ({ items, onCheckBoxChange }) => {
+  const handleCheckboxChange = (id: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    onCheckBoxChange(id, event.target.checked);
+  };
+  return (
+    <div>
+      <table style={{ border: "2px solid black", width: "50%"}}>
+        <thead>
+          <tr>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Items</th>
+            <th style={{ border: "1px solid black", padding: "8px" }}>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr key={item.id}>
+              <td style={ { border: "1px solid black", padding: "8px" }}>
+                <span style={{ marginLeft: "10px"}}>
+                  {item.isChecked ? <del style={{color:"red"}}>{item.title}</del> : item.title}
+                </span>
+                </td>
+                <td style={{textAlign:"center", border: "1px solid black", padding: "8px"}}>
+                <input
+                  type="checkbox"
+                  checked={item.isChecked}
+                  onChange={handleCheckboxChange(item.id)}
+                 
+                />
+             </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
